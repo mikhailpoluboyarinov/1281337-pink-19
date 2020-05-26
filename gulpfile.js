9d344fb3cdbfa1feb62426f727b48bfd7c01568c
Mikhail <mars2109@mail.ru>
2020-05-26 22:24:27 +0300

    Выполнил оставшиеся модули, почистил разметку и стили

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,16 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var webp = require("gulp-webp")
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var htmlmin = require("gulp-htmlmin");
+var minifyjs = require("gulp-js-minify");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -16,14 +26,17 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
+    browser: "google chrome",
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +44,86 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/sprite-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/*.js", gulp.series("refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sprite-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img/webp"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("minify", function () {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task("minify-js", function (done) {
+  gulp.src("source/js/*.js")
+    .pipe(minifyjs())
+    .pipe(gulp.dest("build/js/"));
+  done();
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "minify-js",
+  "sprite",
+  "minify",
+  "html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by M.Poluboyarinov on 26.05.2020.
- */
+// открытие/закрытие меню
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+var colorHeader = document.querySelector(".page-header__control");
+
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
+colorHeader.classList.add('page-header__background');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.add('page-header__toggle--opened');
+    colorHeader.classList.remove("page-header__background");
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.remove('page-header__toggle--opened');
+    colorHeader.classList.add("page-header__background");
+  }
+});
